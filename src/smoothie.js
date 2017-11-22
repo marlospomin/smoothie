b3d01236e2b02c58115deceaebff0eb7e1a7c7c5
Marlos Pomin <marlospomin@gmail.com>
2017-11-22 21:18:57 -0200

    Update smoothie

---
+++ src/smoothie.js
@@ -0,0 +1,10 @@
+// Get element via custom tag
+// Get href value from target
+// Create an event listener on click
+// Upon click slideIntoView smoothly
+// If slideIntoView is not supported use polyfill
+// Add custom options for behavior and custom tag
+
+// Make sure hash is correct
+// Update polyfill to es6 if possible
+// Test headlessly with karma and mocha
