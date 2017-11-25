b085dd5a462f18d692f254556bc4622e7a7cb8e4
Marlos Pomin <marlospomin@gmail.com>
2017-11-24 22:25:14 -0200

    Update smoothie

--- src/smoothie.js
+++ src/smoothie.js
@@ -5,31 +5,36 @@
 // If slideIntoView is not supported use polyfill
 // Add custom options for behavior and custom tag
 
-// Make sure hash is correct
+// Make sure hash is correct - OK
 // Update polyfill to es6 if possible
 // Test headlessly with karma and mocha
 // Refactor to use methods and functions
+// Refactor the hash change method if needed
 // Refactor to remove closure and overall cleanup
 
 // Actual plugin
-// Get the elements marked for smooth scrolling
-const elements = document.querySelectorAll('[data-smoothie]');
-// Save the href value
-let link;
-// For each element get href value
-elements.forEach((element) => {
-  // Save the href value from each element
-  link = element.getAttribute('href');
-  // Wrap as a closure
-  ((link) => {
-    element.addEventListener('click', (event) => {
-      // Prevent default behavior
-      event.preventDefault();
-      // Scroll into view the element
-      document.querySelector(link).scrollIntoView({
-        // Set the animation method
-        behavior: 'smooth'
+export default function () {
+  // Get the elements marked for smooth scrolling
+  const elements = document.querySelectorAll('[data-smoothie]');
+  // Save the href value
+  let link;
+  // For each element get href value
+  elements.forEach((element) => {
+    // Save the href value from each element
+    link = element.getAttribute('href');
+    // Wrap as a closure
+    ((link) => {
+      element.addEventListener('click', (event) => {
+        // Prevent default behavior
+        event.preventDefault();
+        // Load proper hash in the url
+        history.pushState(null, null, link);
+        // Scroll into view the element
+        document.querySelector(link).scrollIntoView({
+          // Set the animation method
+          behavior: 'smooth'
+        });
       });
-    });
-  })(link);
-});
+    })(link);
+  });
+}
