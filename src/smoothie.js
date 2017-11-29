f7e339fc184bacea9b3cec8029b2b2b6a7ba982d
Marlos Pomin <marlospomin@gmail.com>
2017-11-29 20:38:35 -0200

    Refactor smoothie

--- src/smoothie.js
+++ src/smoothie.js
@@ -1,25 +1,21 @@
 export default function () {
   // Get the elements marked for smooth scrolling
   const elements = document.querySelectorAll('[data-smoothie]');
-  // Save the href value
-  let link;
   // For each element get href value
   Array.from(elements).forEach((element) => {
     // Save the href value from each element
-    link = element.getAttribute('href');
-    // Wrap as a closure
-    ((link) => {
-      element.addEventListener('click', (event) => {
-        // Prevent default behavior
-        event.preventDefault();
-        // Load proper hash in the url
-        history.pushState(null, null, link);
-        // Scroll into view the element
-        document.querySelector(link).scrollIntoView({
-          // Set the animation method
-          behavior: 'smooth'
-        });
+    let link = element.getAttribute('href');
+    // Create the click events
+    element.addEventListener('click', (event) => {
+      // Prevent default behavior
+      event.preventDefault();
+      // Load proper hash in the url
+      history.pushState(null, null, link);
+      // Scroll into view the element
+      document.querySelector(link).scrollIntoView({
+        // Set the animation method
+        behavior: 'smooth'
       });
-    })(link);
+    });
   });
 }
