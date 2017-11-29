234934bbec84dedebfdd70678c9fbef40f7ab62a
Marlos Pomin <marlospomin@gmail.com>
2017-11-29 20:38:59 -0200

    Update smoothie

--- dist/smoothie.js
+++ dist/smoothie.js
@@ -20,26 +20,22 @@
   exports.default = function () {
     // Get the elements marked for smooth scrolling
     var elements = document.querySelectorAll('[data-smoothie]');
-    // Save the href value
-    var link = void 0;
     // For each element get href value
     Array.from(elements).forEach(function (element) {
       // Save the href value from each element
-      link = element.getAttribute('href');
-      // Wrap as a closure
-      (function (link) {
-        element.addEventListener('click', function (event) {
-          // Prevent default behavior
-          event.preventDefault();
-          // Load proper hash in the url
-          history.pushState(null, null, link);
-          // Scroll into view the element
-          document.querySelector(link).scrollIntoView({
-            // Set the animation method
-            behavior: 'smooth'
-          });
+      var link = element.getAttribute('href');
+      // Create the click events
+      element.addEventListener('click', function (event) {
+        // Prevent default behavior
+        event.preventDefault();
+        // Load proper hash in the url
+        history.pushState(null, null, link);
+        // Scroll into view the element
+        document.querySelector(link).scrollIntoView({
+          // Set the animation method
+          behavior: 'smooth'
         });
-      })(link);
+      });
     });
   };
 
