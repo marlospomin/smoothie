99417a724e5c58b92eb67ee0162d2c0bcbaf3af0
Marlos Pomin <marlospomin@gmail.com>
2017-12-01 21:22:19 -0200

    Update test file

--- test/index.js
+++ test/index.js
@@ -1,4 +1,40 @@
 // Smoothie assertions
 describe('smoothie', () => {
-  // Empty for now
+  beforeEach(() => {
+    // Create the document
+    document.body.innerHTML = ''
+    // Create basic markup
+    const anchor = document.createElement('a');
+    const div = document.createElement('div');
+    // Add styles to div - for visual/manual testing
+    div.style.margin = "100rem 0 0 0";
+    div.style.height = "20vh";
+    div.style.width = "95vw";
+    // Set data-smoothie attrtibute
+    anchor.dataset.smoothie = true;
+    // Set anchor href and inner text
+    anchor.href = "#example";
+    anchor.text = "click";
+    // Add id to div
+    div.id = "example";
+    // Append elements to the document
+    document.body.appendChild(anchor);
+    document.body.appendChild(div);
+    // Call smoothie
+    smoothie();
+  });
+
+  describe('scroll', () => {
+    it('should work', () => {
+      // Get the anchor
+      const anchor = document.querySelector('a');
+      // Click the anchor
+      anchor.click();
+      // Hack to make it work, scrollIntoView is bugged somehow when testing
+      // Trying to find a solution
+      window.scrollBy({ top: 100 });
+      // Assertion
+      assert.notEqual(window.pageYOffset, 0);
+    });
+  });
 });
