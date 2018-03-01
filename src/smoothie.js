cda57c591452f4e1822f79b8681103daa018a0a7
Marlos Pomin <marlospomin@gmail.com>
2018-03-01 20:33:04 -0300

    Drop old syntax

--- src/smoothie.js
+++ src/smoothie.js
@@ -3,27 +3,25 @@ const defaultConfig = {
   block: 'start',
   // Animation method
   behavior: 'smooth'
-};
+}
 
 export default function (config = {}) {
   // Push defaults into config object
-  config = { ...defaultConfig, ...config };
-
+  config = { ...defaultConfig, ...config }
   // Get the elements marked for smooth scrolling
-  const elements = document.querySelectorAll('[data-smoothie]');
-
+  const elements = document.querySelectorAll('[data-smoothie]')
   // For each element get href value
   Array.from(elements).forEach((element) => {
     // Save the href value from each element
-    let link = element.getAttribute('href');
+    let link = element.getAttribute('href')
     // Create the click events
     element.addEventListener('click', (event) => {
       // Prevent default behavior
-      event.preventDefault();
+      event.preventDefault()
       // Load proper hash in the url
-      history.pushState(null, null, link);
+      history.pushState(null, null, link)
       // Scroll into view the element
-      document.querySelector(link).scrollIntoView(config);
-    });
-  });
+      document.querySelector(link).scrollIntoView(config)
+    })
+  })
 }
--- test/index.js
+++ test/index.js
@@ -1,40 +1,39 @@
-// Smoothie assertions
 describe('smoothie', () => {
   beforeEach(() => {
     // Create the document
     document.body.innerHTML = ''
     // Create basic markup
-    const anchor = document.createElement('a');
-    const div = document.createElement('div');
+    const anchor = document.createElement('a')
+    const div = document.createElement('div')
     // Add styles to div - for visual/manual testing
-    div.style.margin = "100rem 0 0 0";
-    div.style.height = "20vh";
-    div.style.width = "95vw";
+    div.style.margin = "100rem 0 0 0"
+    div.style.height = "20vh"
+    div.style.width = "95vw"
     // Set data-smoothie attrtibute
-    anchor.dataset.smoothie = true;
+    anchor.dataset.smoothie = true
     // Set anchor href and inner text
-    anchor.href = "#example";
-    anchor.text = "click";
+    anchor.href = "#example"
+    anchor.text = "click"
     // Add id to div
-    div.id = "example";
+    div.id = "example"
     // Append elements to the document
-    document.body.appendChild(anchor);
-    document.body.appendChild(div);
+    document.body.appendChild(anchor)
+    document.body.appendChild(div)
     // Call smoothie
-    smoothie();
-  });
+    smoothie()
+  })
 
   describe('scroll', () => {
     it('should work', () => {
       // Get the anchor
-      const anchor = document.querySelector('a');
+      const anchor = document.querySelector('a')
       // Click the anchor
-      anchor.click();
+      anchor.click()
       // Hack to make it work, scrollIntoView is bugged somehow when testing
       // Trying to find a solution
-      window.scroll(0, 1);
+      window.scroll(0, 1)
       // Assertion
-      assert.notEqual(window.pageYOffset, 0);
-    });
-  });
-});
+      assert.notEqual(window.pageYOffset, 0)
+    })
+  })
+})
