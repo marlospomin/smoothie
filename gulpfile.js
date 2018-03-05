1ba33d17d7000e9f6f11d1d7ef5e4f5fa446bb88
Marlos Pomin <marlospomin@gmail.com>
2018-03-04 21:09:20 -0300

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,33 +3,6 @@ const babel = require('gulp-babel');
 const rename = require("gulp-rename");
 const karma = require('karma').Server;
 const uglify = require('gulp-uglifyjs');
-const browserSync = require('browser-sync');
-
-// Browser-sync task
-gulp.task('server', function() {
-  browserSync.init({
-    server: "./example/"
-  });
-
-  // Watch tasks
-  gulp.watch(['./src/*.js'], ['reload']);
-  gulp.watch(['./example/*.html', './example/js/*.js', './example/css/*.css'],
-    browserSync.reload);
-});
-
-// Reload task
-gulp.task('reload', function() {
-  gulp.src(['./src/smoothie.js'])
-    .pipe(babel({
-      presets: ['env'],
-      plugins: ['add-module-exports', 'transform-object-rest-spread',
-        'transform-es2015-modules-umd'
-      ]
-    }))
-    .pipe(uglify())
-    .pipe(rename('smoothie.min.js'))
-    .pipe(gulp.dest('./example/js'))
-});
 
 // Test task
 gulp.task('test', function (done) {
@@ -57,4 +30,4 @@ gulp.task('build', function() {
 });
 
 // Set the defaukt task as server
-gulp.task('default', ['server']);
+gulp.task('default', ['build']);
